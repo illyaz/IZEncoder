45dc46b86e9f6e6361ab36e1f977990b6936bac6
illyaz <26743376+illyaz@users.noreply.github.com>
2022-01-29 21:49:19 +0700

    Update publish script

--- build/publish.bat
+++ build/publish.bat
@@ -1,3 +1,2 @@
-.\utility\IZEncoder.Server.Utility.exe --makedist .\distlist.json --baseDir .\x64\Release\ --outDir .\publish\ --zip publish.zip
-REM .\utility\IZEncoder.Server.Utility.exe --makedist .\distlist.json --baseDir .\x64\Release\ --outDir "W:\public_html\static\ize"
+.\utility\IZEncoder.Server.Utility.exe --makedist .\distlist.json --baseDir .\x64\Release\ --outDir .\publish
 pause
